a685723b4694deab9844459a8134dd4b618c2a7c
Kapoor-0905 <500097111@stu.upes.ac.in>
2023-01-07 09:24:11 +0530

    Flutter Boilerplate and Firebase auth

---
+++ lib/main.dart
@@ -0,0 +1,60 @@
+// import 'package:event_planning_app/constants/constants.dart';
+// import 'package:event_planning_app/pages/login.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/foundation.dart';
+// import 'package:event_planning_app/pages/home.dart';
+// import 'package:event_planning_app/pages/onBoard.dart';
+// import 'package:event_planning_app/helper/helperFunctions.dart';
+import 'package:firebase_core/firebase_core.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // if (kIsWeb) {
+  //   await Firebase.initializeApp(
+  //       options: FirebaseOptions(
+  //           apiKey: Constants.apiKey,
+  //           appId: Constants.appId,
+  //           messagingSenderId: Constants.messagingSenderId,
+  //           projectId: Constants.projectId));
+  // } else {
+  //   await Firebase.initializeApp();
+  // }
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  bool _isSignedIn = false;
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    //getUserLoggedInStatus();
+  }
+
+  // getUserLoggedInStatus() async {
+  //   await HelperFunctions.getUserLoggedInStatus().then((value) {
+  //     setState(() {
+  //       _isSignedIn = value!;
+  //     });
+  //   });
+  // }
+
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Event Planning App',
+      theme: ThemeData(
+        //primaryColor: Constants.primaryColor,
+      ),
+      //home: _isSignedIn ? const Home() : const Login(),
+    );
+  }
+}
